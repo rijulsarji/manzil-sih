6178b0cc4400bc3a480d1fce2ce2af089ad2eda4
Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
2022-03-18 14:34:45 +0530

    modified courses page

--- src/Pages/courses.js
+++ src/Pages/courses.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../CSS/Pages/courses.css";
+import signLang from "../assets/signLang.png"
 
 function Courses() {
 
@@ -9,6 +10,13 @@ function Courses() {
         <div className="courseHead">
           <h1>Courses</h1>
           <p>Hello world</p>
+          <div className="courseCard">
+            <img src={signLang} alt="card" />
+            <h2>
+              Number 1 to 9 | Sign Language Interpretation
+            </h2>
+            <p>FREE</p>
+          </div>
         </div>
       </div>
     </div>
